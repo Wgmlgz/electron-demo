76fa0cff6fc51bc420846aa634c921855c8f8aa4
Wgmlgz <62064707+Wgmlgz@users.noreply.github.com>
2023-05-25 23:33:54 +0300

    lambda stats

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,8 @@
+export const MIN_SCALE = 0.1;
+
 type ActionStatus = '+' | '-';
 type Action = `${string}${ActionStatus}`;
+
 export interface Device {
   date: Date;
   deviceId: string;
@@ -9,4 +12,10 @@ export interface Device {
   actions: Action[];
 }
 
-export const MIN_SCALE = 0.1;
+export interface LambdaStats {
+  lambda_inactivity: number;
+  max_lambda_inactivity: number;
+  queuing_time: number;
+  max_queuing_time: number;
+  shard: string;
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import DeviceComponent from '$lib/device.svelte';
-  import type { Device } from '$lib/utils';
+  import LambdaStatsComponent from '$lib/lambda-stats.svelte';
+  import type { Device, LambdaStats } from '$lib/utils';
   import createPanZoom, { type PanZoom } from 'panzoom';
   import { onMount } from 'svelte';
 
   interface DeviceData {
     devices: Device[];
+    lambda_stats: LambdaStats[];
   }
   const data: DeviceData = {
     devices: [
@@ -33,6 +35,29 @@
         shardId: '0003',
         actions: ['S+', 'C+', '3+', 'U-']
       }
+    ],
+    lambda_stats: [
+      {
+        lambda_inactivity: 12,
+        max_lambda_inactivity: 15,
+        queuing_time: 20,
+        max_queuing_time: 30,
+        shard: 'shard1'
+      },
+      {
+        lambda_inactivity: 11,
+        max_lambda_inactivity: 20,
+        queuing_time: 15,
+        max_queuing_time: 35,
+        shard: 'shard_2'
+      },
+      {
+        lambda_inactivity: 22,
+        max_lambda_inactivity: 25,
+        queuing_time: 10,
+        max_queuing_time: 20,
+        shard: 'shard_3'
+      },
     ]
   };
   let zoomer: HTMLElement;
@@ -50,6 +75,10 @@
   });
 </script>
 
+<div class="h-400px w-full">
+  <LambdaStatsComponent  bind:lambda_stats={data.lambda_stats}/>
+</div>
+
 <div class="h-screen w-screen absolute overflow-clip">
   <div
     on:scroll={(e) => {
@@ -66,9 +95,8 @@
       class="text-center h-full w-full grid items-center justify-items-center"
     >
       <div class="flex gap-10">
-
         {#each data.devices as device}
-        <DeviceComponent {scale} bind:device />
+          <DeviceComponent {scale} bind:device />
         {/each}
       </div>
     </div>
